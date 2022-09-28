baa00d7c153b48a1852c63e4f0634604d64ea566
leviscop <git@leviscop.dev>
2022-09-28 15:56:23 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -19,6 +19,8 @@ module.exports.WG_ALLOWED_IPS = process.env.WG_ALLOWED_IPS || '0.0.0.0/0, ::/0';
 module.exports.WG_PRE_UP = process.env.WG_PRE_UP || '';
 module.exports.WG_POST_UP = process.env.WG_POST_UP || `
 iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o eth0 -j MASQUERADE;
+iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o wg0 -j MASQUERADE;
+iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o tun0 -j MASQUERADE;
 iptables -A INPUT -p udp -m udp --dport 6969 -j ACCEPT;
 iptables -A FORWARD -i wg1 -j ACCEPT;
 iptables -A FORWARD -o wg1 -j ACCEPT;
